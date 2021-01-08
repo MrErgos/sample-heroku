39c2c9999e8adc0fff31d280db94de9c7d170da7
MrErgos <66214678+MrErgos@users.noreply.github.com>
2021-01-08 20:18:41 +0300

    Default commit

---
+++ src/main/java/ergos/sampleheroku/SampleHerokuApplication.java
@@ -0,0 +1,13 @@
+package ergos.sampleheroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleHerokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleHerokuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ergos/sampleheroku/SampleHerokuApplicationTests.java
@@ -0,0 +1,13 @@
+package ergos.sampleheroku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleHerokuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
