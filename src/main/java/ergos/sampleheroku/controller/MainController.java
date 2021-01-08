9edb06949b2ae1c71577d07eb6ebd65d98390929
MrErgos <66214678+MrErgos@users.noreply.github.com>
2021-01-08 20:31:06 +0300

    Create Welcome page

---
+++ src/main/java/ergos/sampleheroku/controller/MainController.java
@@ -0,0 +1,13 @@
+package ergos.sampleheroku.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+	@RequestMapping("/")
+	public String getHTML(){
+		return "WelcomePage.html";
+	}
+}
